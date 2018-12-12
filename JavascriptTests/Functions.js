ed19ec29893c0ff15045c740d949e4c4db926a4e
Abdul Qadir <qad3r89@gmail.com>
2018-12-12 08:51:55 +0000

    Array shuffle and word order reverse, all test cases passing

--- JavascriptTests/Functions.js
+++ JavascriptTests/Functions.js
@@ -19,15 +19,34 @@ function Question2(inputArray) {
 function Question3(inputArray, itemToFind) {
 	// COUNT THE NUMBER OF ELEMENTS IN AN ARRAY
 	// You should write a function that returns a count of the number of 'itemToFind' that exist in 'inputArray'
-	
-	return undefined;
+	var count = 0;
+    for (var i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] === itemToFind) {
+            count++;
+        }
+    }
+	return count;
 }
 
 function Question4(deckOfCardsArray) {
 	// SHUFFLE A DECK OF CARDS
 	// You should write a function that shuffles the items in 'deckOfCardsArray'
 	// The original array should be modified
+	var currentIndex = deckOfCardsArray.length, temporaryValue, randomIndex;
 
+	  // While there remain elements to shuffle...
+	  while (currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+
+	    temporaryValue = deckOfCardsArray[currentIndex];
+	    deckOfCardsArray[currentIndex] = deckOfCardsArray[randomIndex];
+	    deckOfCardsArray[randomIndex] = temporaryValue;
+	  }
 	// No return value needed
 }
 
@@ -36,6 +55,6 @@ function Question5(inputString) {
 	// You should write a function that reverses the order of the words in a string. The letters in
 	// the words should be unchanged.
 	// e.g. "the cat sat on the mat" should become "mat the on sat cat the"
-	
-	return undefined;
+	var reversed = inputString.split(' ').reverse().join(' ');
+	return reversed;
 }
